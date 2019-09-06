1dfb7fdfcd4ced2258f6beb1b3d7391e14102c61
BW05KangChengYu <158596764@qq.com>
2019-09-06 13:49:16 +0800

    初始化版本

---
+++ src/test/java/com/bwie/test/TestUtils.java
@@ -0,0 +1,53 @@
+package com.bwie.test;
+
+import org.junit.Test;
+
+import com.bwie.kangchengyu.StrUtils;
+
+/**
+ * 
+ * @author apple
+ *
+ */
+public class TestUtils {
+	@Test
+	public void testIsNull() {
+		String tel1=" ";
+		String tel2="haha";
+		boolean tel3 = StrUtils.isNull(tel1);
+		boolean tel4 = StrUtils.isNull(tel2);
+		System.out.println(tel3);
+		System.out.println(tel4);
+	}
+	@Test
+	public void testTel() {
+		String tel1="13791603997";
+		String tel2="12221212121";
+		boolean tel3 = StrUtils.isTelephone(tel1);
+		boolean tel4 = StrUtils.isTelephone(tel2);
+		System.out.println(tel3);
+		System.out.println(tel4);
+	}
+	@Test
+	public void testEmail() {
+		String tel1="13791603997@qq.com";
+		String tel2="12221212121@mm.a";
+		boolean tel3 = StrUtils.isEmail(tel1);
+		boolean tel4 = StrUtils.isEmail(tel2);
+		System.out.println(tel3);
+		System.out.println(tel4);
+	}
+	@Test
+	public void testLetter() {
+		String tel1="asdffgdAs";
+		String tel2="1csasaA";
+		boolean tel3 = StrUtils.isLetter(tel1);
+		boolean tel4 = StrUtils.isLetter(tel2);
+		System.out.println(tel3);
+		System.out.println(tel4);
+	}
+	@Test
+	public void testCn() {
+		System.out.println(StrUtils.getRandomCnStr(4));
+	}
+}
