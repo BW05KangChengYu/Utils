4aeea05fea4c515b541e2740db4d7ffd371ba25a
BW05KangChengYu <158596764@qq.com>
2019-09-09 08:01:05 +0800

    zuizhong

--- src/main/java/com/bwie/kangchengyu/StrUtils.java
+++ src/main/java/com/bwie/kangchengyu/StrUtils.java
@@ -196,7 +196,7 @@ public class StrUtils {
 	 */
 	public static String toHtml(String src) {
 		
-		String[] strings = src.split("\\\\n");
+		String[] strings = src.split("\\\n");
 		StringBuilder sb = new StringBuilder();
 		for (String string : strings) {
 			sb.append("<p>").append(string).append("</p>");
